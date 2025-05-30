81fe537af4b16505908478c7f635d52149cd7a73
Rakaaltamis <rakaaltmizz@gmail.com>
2025-05-30 19:15:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,6 @@ import streamlit as st
 st.title("WELCOME")
 st.write("SELAMAT DATANG DI WEB RAKA")
 st.header("sikatt miring")
-st.image("IMG-20250529-WA0001.jpg"
-        )
+
+st.image("IMG-20250529-WA0001.jpg")
+        
